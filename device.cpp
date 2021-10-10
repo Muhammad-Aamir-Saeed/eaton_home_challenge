6051883db47bf8395957eb2400119dfcc01a2624
Muhammad-Aamir-Saeed <m.aamir.s.110@gmail.com>
2021-10-10 15:31:19 +0300

    did not use autoformatter

--- device.cpp
+++ device.cpp
@@ -8,41 +8,42 @@
 #include <string>
 #include <random>
 
-float get_random()	//to generate random measurements;
+float get_random()//to generate random measurements;
 {
-	// Random seed
-	std::random_device rd;
-	// Initialize Mersenne Twister pseudo-random number generator
-	std::mt19937 gen(rd());
-	static std::uniform_real_distribution < > dis(0, 100);	// rage 0 - 100
-	return dis(gen);
+    // Random seed
+    std::random_device rd;
+    // Initialize Mersenne Twister pseudo-random number generator
+    std::mt19937 gen(rd());
+    static std::uniform_real_distribution<> dis(0, 100); // rage 0 - 100
+    return dis(gen);
 }
 
-int main(int argc, char **argv)
+
+int main(int argc, char ** argv)
 {
-	uint8_t deviceid;	//get device id from the user
+	uint8_t deviceid; //get device id from the user
 	int port;
 	std::vector<float> measurements;
 	int sock = -1;
 	struct sockaddr_in address;
 	struct hostent * host;
 	int len;
-
-	/*checking commandline parameter */
+	
+	/* checking commandline parameter */
 	if (argc != 5)
 	{
-		std::cout << "usage: " << argv[0] << " hostname port_no number_of_measurements device_id\n";
+		std::cout<<"usage: "<<argv[0]<< " hostname port_no number_of_measurements device_id\n";
 		return -1;
 	}
 
-	/*obtain port number */
+	/* obtain port number */
 	if (sscanf(argv[2], "%d", &port) <= 0)
 	{
 		fprintf(stderr, "%s: error: wrong parameter: port\n", argv[0]);
 		return -2;
 	}
 
-	/*create socket */
+	/* create socket */
 	sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
 	if (sock <= 0)
 	{
@@ -50,45 +51,48 @@ int main(int argc, char **argv)
 		return -3;
 	}
 
-	/*connect to server */
+	/* connect to server */
 	address.sin_family = AF_INET;
 	address.sin_port = htons(port);
-	host = gethostbyname(argv[1]);	//get the host name
+	host = gethostbyname(argv[1]); //get the host name
 	if (!host)
 	{
 		fprintf(stderr, "%s: error: unknown host %s\n", argv[0], argv[1]);
 		return -4;
 	}
 	memcpy(&address.sin_addr, host->h_addr_list[0], host->h_length);
-	if (connect(sock, (struct sockaddr *) &address, sizeof(address)))
+	if (connect(sock, (struct sockaddr *)&address, sizeof(address)))
 	{
 		fprintf(stderr, "%s: error: cannot connect to host %s\n", argv[0], argv[1]);
 		return -5;
 	}
-
+	
 	//send it as one one long string that is composed of the device id and the measurements
 	//the header file can be 8 bits to compose the device id followed by the remaining measurements
-	/*number of measurements to send to the server */
+	/* number of measurements to send to the server */
 	int no_measurements = atoi(argv[3]);
 
-	deviceid = atoi(argv[4]);	//get the device id from the user
+	
+	
+	deviceid = atoi(argv[4]);//get the device id from the user
 	std::string buffer = std::to_string(deviceid);
-
+	
 	//generate random measurements and store them in a vector and then convert them to a string to be send
 	for (int i = 0; i < no_measurements; ++i)
 	{
 		buffer += ":";
 		measurements.push_back(get_random());
 		buffer += std::to_string(measurements.at(i));
+
 	}
 
-	std::cout << "data that is send to server: " << buffer << std::endl;
+	std::cout <<"data that is send to server: "<< buffer << std::endl;
 	//std::cout << buffer.size() << std::endl;
-	uint32_t dataLength = htonl(buffer.size());	// Ensure network byte order when sending the data length
-	write(sock, &dataLength, sizeof(uint32_t));	//first send the number of bytes the server will receive
-	write(sock, buffer.c_str(), buffer.size());	//then send the actual data
+	uint32_t dataLength = htonl(buffer.size()); // Ensure network byte order when sending the data length
+	write(sock, &dataLength, sizeof(uint32_t));//first send the number of bytes the server will receive
+	write(sock, buffer.c_str(), buffer.size());//then send the actual data
 
-	/*close socket */
+	/* close socket */
 	close(sock);
 
 	return 0;
--- server.cpp
+++ server.cpp
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
-#include <netinet/in.h >	// struct sockaddr_in
-#include <arpa/inet.h >	// htons(), htonl()
+#include <netinet/in.h> // struct sockaddr_in
+#include <arpa/inet.h> // htons(), htonl()
 #include <sys/socket.h>
 #include <linux/in.h>
 #include <unistd.h>
@@ -24,30 +24,29 @@ typedef struct
 	int sock;
 	struct sockaddr address;
 	int addr_len;
-}
-connection_t;
+} connection_t;
 
 //decode the messages that have been send by the various devices
-void message_decode(const std::string &buffer, uint8_t &device_id, size_t &measurementcount)
-{
+void message_decode(const std::string &buffer,uint8_t &device_id,size_t &measurementcount)
+{	
 	std::string s = buffer;
-	std::vector<float> measurements;	//TODO make this a global or pass it to the main thread, currently gets deleted when the function is ended
+	std::vector<float> measurements;//TODO make this a global or pass it to the main thread, currently gets deleted when the function is ended
 	std::string delim = ":";
-	auto start = 0 U;
+	auto start = 0U;
 	auto end = s.find(delim);
 
-	while (end != std::string::npos)
-	{
-		if (measurementcount == 0)	//the first string contains the device id
+   while (end != std::string::npos)
+  {
+		if(measurementcount==0)//the first string contains the device id
 		{
-			device_id = (uint8_t) atoi((s.substr(start, end - start)).c_str());
+			device_id = (uint8_t)atoi((s.substr(start, end - start)).c_str());
 		}
 		measurements.push_back(stof(s.substr(start, end - start)));
 
 		start = end + delim.length();
 		end = s.find(delim, start);
 		measurementcount++;
-	}
+  }	    
 	measurements.push_back(stof(s.substr(start, end - start)));
 	/*to print the individual measurements of the message that has been send*/
 
@@ -59,71 +58,74 @@ void message_decode(const std::string &buffer, uint8_t &device_id, size_t &measu
 	//{
 	//	std::cout<<measurements.at(i)<<std::endl;
 	//}
-	//std::cout << s.substr(start, end);
-
+		//std::cout << s.substr(start, end);
+	
 }
 
-void *process(void *ptr)
+void * process(void * ptr)
 {
-	char *buffer;
+	char * buffer;
 	int len;
 	connection_t * conn;
 	uint8_t device_id;
 	size_t device_message_count = 0;
 	size_t number_of_measurements = 0;
+	
 
-	if (!ptr) pthread_exit(0);
-	conn = (connection_t*) ptr;
+	if (!ptr) pthread_exit(0); 
+	conn = (connection_t *)ptr;
 
-	/*read length of message */	//some devices can send more than one measurement of data
+	/* read length of message */ //some devices can send more than one measurement of data
 	read(conn->sock, &len, sizeof(int));
-	len = ntohl(len);	// Ensure host system byte order
-	if (len > 0)	//we have successfully received a data packet from a device
+	len = ntohl(len); // Ensure host system byte order
+	if (len > 0)//we have successfully received a data packet from a device
 	{
-		buffer = (char*) malloc((len + 1) *sizeof(char));	//create a buffer so as to store the data
-		buffer[len] = 0;	//make it zero
+		buffer = (char *)malloc((len+1)*sizeof(char)); //create a buffer so as to store the data
+		buffer[len] = 0; //make it zero
 
-		/*read message */
+		/* read message */
 		read(conn->sock, buffer, len);
 
+		
 		//breaking down the message to individual measurements 
-		message_decode(std::string(buffer), device_id, number_of_measurements);
-		std::cout << "device " << (int) device_id << " just send: " << number_of_measurements << " message/s now\n";
-		std::cout << "--------\n";
-
+		message_decode(std::string(buffer),device_id,number_of_measurements);
+		std::cout<<"device "<<(int)device_id<<" just send: "<<number_of_measurements<<" message/s now\n";
+		std::cout<<"--------\n";
+			 
 		free(buffer);
 
 		//lock the threads
 		mtx01.lock();
-		//find which device has previous measurements and update accordingly
-		if (per_device_messages.find(device_id) != per_device_messages.end())
-		{
-			device_message_count = per_device_messages[device_id];
-		}
+			//find which device has previous measurements and update accordingly
+			if (per_device_messages.find(device_id) != per_device_messages.end()) 
+			    {
+					device_message_count = per_device_messages[device_id];
+			    }
 
-		per_device_messages[device_id] = ++device_message_count;
+	    	per_device_messages[device_id] = ++device_message_count;
 
-		//count the number of total measurements that have been received
-		int sum = 0;
+	    	//count the number of total measurements that have been received
+			int sum = 0;
 
-		for (const auto &pair: per_device_messages)
-		{
-			std::cout << "device " << (int) pair.first << " has a send a total of: " << pair.second << " message/s\n";
-			// printf("device %d has send a total of: %ld messages\n", pair.first, pair.second);
-			sum += pair.second;
-		}
-		std::cout << "number of measurements received from all devices: " << sum << std::endl;
-		std::cout << "\n--------------\n";
+			for ( const auto &pair : per_device_messages ) 
+	    		{
+	        		std::cout<<"device "<<(int)pair.first<<" has a send a total of: "<< pair.second<<" message/s\n";
+	        		// printf("device %d has send a total of: %ld messages\n", pair.first, pair.second);
+	        		sum += pair.second;
+	            }
+			std::cout<<"number of measurements received from all devices: "<<sum<<std::endl;
+			std::cout<<"\n--------------\n";
 		mtx01.unlock();
+		
 	}
 
-	/*close socket and clean up */
+	/* close socket and clean up */
 	close(conn->sock);
 	free(conn);
 	pthread_exit(0);
 }
 
-int main(int argc, char **argv)
+int main(int argc, char ** argv)
 {
 	int sock = -1;
 	struct sockaddr_in address;
@@ -131,21 +133,21 @@ int main(int argc, char **argv)
 	connection_t * connection;
 	pthread_t thread;
 
-	/*check for command line arguments */
+	/* check for command line arguments */
 	if (argc != 2)
 	{
 		fprintf(stderr, "usage: %s port\n", argv[0]);
 		return -1;
 	}
 
-	/*obtain port number */
+	/* obtain port number */
 	if (sscanf(argv[1], "%d", &port) <= 0)
 	{
 		fprintf(stderr, "%s: error: wrong parameter: port\n", argv[0]);
 		return -2;
 	}
 
-	/*create socket */
+	/* create socket */
 	sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
 	if (sock <= 0)
 	{
@@ -153,41 +155,42 @@ int main(int argc, char **argv)
 		return -3;
 	}
 
-	/*bind socket to port */
+	/* bind socket to port */
 	address.sin_family = AF_INET;
 	address.sin_addr.s_addr = INADDR_ANY;
 	address.sin_port = htons(port);
-	if (bind(sock, (struct sockaddr *) &address, sizeof(struct sockaddr_in)) < 0)
+	if (bind(sock, (struct sockaddr *)&address, sizeof(struct sockaddr_in)) < 0)
 	{
 		fprintf(stderr, "%s: error: cannot bind socket to port %d\n", argv[0], port);
 		return -4;
 	}
 
-	/*listen on port */
+	/* listen on port */
 	if (listen(sock, 10) < 0)
 	{
 		fprintf(stderr, "%s: error: cannot listen on port\n", argv[0]);
 		return -5;
 	}
 
-	std::cout << argv[0] << ": is up and running\n";
-	std::cout << "--------------\n\n";
-
+	std::cout<<argv[0]<<": is up and running\n";
+	std::cout<<"--------------\n\n";
+	
 	while (1)
 	{
-		/*accept incoming connections */
-		connection = (connection_t*) malloc(sizeof(connection_t));
-		connection->sock = accept(sock, &connection->address, (socklen_t*) &connection->addr_len);
+		/* accept incoming connections */
+		connection = (connection_t *)malloc(sizeof(connection_t));
+		connection->sock = accept(sock, &connection->address, (socklen_t*)&connection->addr_len);
 		if (connection->sock <= 0)
 		{
 			free(connection);
 		}
 		else
-		{ /*start a new thread but do not wait for it */
-			pthread_create(&thread, 0, process, (void*) connection);
+		{
+			/* start a new thread but do not wait for it */
+			pthread_create(&thread, 0, process, (void *)connection);
 			pthread_detach(thread);
 		}
 	}
-
+	
 	return 0;
 }
